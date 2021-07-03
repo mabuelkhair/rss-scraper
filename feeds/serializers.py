6c11c11daecb64280a867601082664c49cb716c8
Mostafa <mabuelkhair94@gmail.com>
2021-07-03 19:40:57 +0200

    create feed serializer

--- feeds/serializers.py
+++ feeds/serializers.py
@@ -5,7 +5,19 @@ from feeds.models import Feed
 
 class FeedSerializer(serializers.ModelSerializer):
 
-    model = Feed
+    class Meta:
+        fields = '__all__'
+        model = Feed
+
+
+class CreateFeedSerializer(serializers.Serializer):
+    url = serializers.URLField()
 
     class Meta:
         fields = '__all__'
+
+    def validate_url(self, value):
+        user = self.context['request'].user
+        if Feed.objects.filter(owner=user, xml_link=value).exists():
+            raise serializers.ValidationError("You already following this RSS")
+        return value
