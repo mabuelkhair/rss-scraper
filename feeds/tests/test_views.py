881e08f85943f45eee60618a7d9cb5ed54b0d7ac
Mostafa <mabuelkhair94@gmail.com>
2021-07-12 03:19:25 +0200

    test items viewSet

--- feeds/tests/test_views.py
+++ feeds/tests/test_views.py
@@ -240,3 +240,157 @@ class TestFeedViewSet(APITestCase):
         self.assertEqual('Invalid feed', json_data.get('errors')[0])
         feed.refresh_from_db()
         self.assertFalse(feed.updated)
+
+
+class TestItemViewSet(APITestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.client = APIClient()
+        refresh = RefreshToken.for_user(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {refresh.access_token}')
+        self.feed = FeedFactory(owner=self.user)
+
+    def test_list_items(self):
+        ItemFactory.create_batch(4, feed=self.feed)
+        response = self.client.get(
+            '/items/',
+            content_type='application/json'
+        )
+        json_data = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json_data.get("count"), 4)
+        self.assertEqual(len(json_data.get("results")), 4)
+        for result in json_data.get("results"):
+            item = Item.objects.get(id=result.get('id'))
+            self.assertEqual(result.get('title'), item.title)
+            self.assertEqual(result.get('link'), item.link)
+            self.assertEqual(result.get('read'), item.read)
+
+    def test_list_all_feeds_items(self):
+        feed2 = FeedFactory(owner=self.user)
+        ItemFactory.create_batch(4, feed=self.feed)
+        ItemFactory.create_batch(4, feed=feed2)
+        response = self.client.get(
+            '/items/',
+            content_type='application/json'
+        )
+        json_data = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json_data.get("count"), 8)
+        self.assertEqual(Feed.objects.count(), 2)
+        self.assertEqual(Item.objects.count(), 8)
+
+    def test_list_only_owned_feeds_items(self):
+        ItemFactory.create_batch(4, feed=self.feed)
+        ItemFactory.create_batch(4)
+        response = self.client.get(
+            '/items/',
+            content_type='application/json'
+        )
+        json_data = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json_data.get("count"), 4)
+        self.assertEqual(Item.objects.count(), 8)
+
+        for result in json_data.get("results"):
+            item = Item.objects.get(id=result.get('id'))
+            self.assertEqual(result.get('feed'), self.feed.id)
+            self.assertEqual(item.feed_id, self.feed.id)
+
+    def test_filter_list_items(self):
+        ItemFactory.create_batch(4, feed=self.feed, read=True)
+        ItemFactory.create_batch(3, feed=self.feed, read=False)
+        response = self.client.get(
+            '/items/?read=true',
+            content_type='application/json'
+        )
+        json_data = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json_data.get("count"), 4)
+        self.assertEqual(len(json_data.get("results")), 4)
+        self.assertEqual(Item.objects.count(), 7)
+        for result in json_data.get("results"):
+            item = Item.objects.get(id=result.get('id'))
+            self.assertTrue(item.read)
+
+        response = self.client.get(
+            '/items/?read=false',
+            content_type='application/json'
+        )
+        json_data = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json_data.get("count"), 3)
+        self.assertEqual(len(json_data.get("results")), 3)
+        for result in json_data.get("results"):
+            item = Item.objects.get(id=result.get('id'))
+            self.assertFalse(item.read)
+
+    def test_list_feed_items(self):
+        feed2 = FeedFactory(owner=self.user)
+        ItemFactory.create_batch(4, feed=self.feed)
+        ItemFactory.create_batch(4, feed=feed2)
+        response = self.client.get(
+            '/feeds/{}/items/'.format(self.feed.pk),
+            content_type='application/json'
+        )
+        json_data = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json_data.get("count"), 4)
+        self.assertEqual(Item.objects.count(), 8)
+        for result in json_data.get("results"):
+            item = Item.objects.get(id=result.get('id'))
+            self.assertEqual(self.feed.pk, item.feed_id)
+            self.assertEqual(self.feed.pk, result.get('feed'))
+
+    def test_access_other_user_feed_items(self):
+        feed = FeedFactory()
+        ItemFactory.create_batch(4, feed=feed)
+        response = self.client.get(
+            '/feeds/{}/items/'.format(feed.pk),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 403)
+
+    def test_mark_feed_items_read(self):
+        items = ItemFactory.create_batch(3, feed=self.feed, read=False)
+        response = self.client.post(
+            '/feeds/{}/items/read/'.format(self.feed.pk),
+            data=json.dumps({"ids": [items[0].id, items[1].id]}),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 200)
+        for item in items:
+            item.refresh_from_db()
+        self.assertTrue(items[0].read)
+        self.assertTrue(items[1].read)
+        self.assertFalse(items[2].read)
+
+    def test_mark_items_read(self):
+        items = ItemFactory.create_batch(3, feed=self.feed, read=False)
+        response = self.client.post(
+            '/items/read/',
+            data=json.dumps({"ids": [items[0].id, items[1].id]}),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 200)
+        for item in items:
+            item.refresh_from_db()
+        self.assertTrue(items[0].read)
+        self.assertTrue(items[1].read)
+        self.assertFalse(items[2].read)
+
+    def test_mark_other_user_item_as_read(self):
+        item = ItemFactory.create(read=False)
+        response = self.client.post(
+            '/items/read/',
+            data=json.dumps({"ids": [item.id]}),
+            content_type='application/json'
+        )
+        json_data = response.json()
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            json_data.get('ids'),
+            ['Some ids are invalid or you do not have permission to edit']
+        )
+        item.refresh_from_db()
+        self.assertFalse(item.read)
